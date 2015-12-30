865670e15a06a313c0ebbb97f6c5b3d0a85b4309
Audrey Cole <audreycole88@yahoo.com>
2015-12-30 11:07:41 -0800

    First commit

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Configure labels
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        //var billAmount = Double(billField.text!)
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        // Assign label values
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        // Want 2 significant figures for $ amount
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f",total)
+        
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        // Turn off the keyboard if you tap anywhere on the screen
+        view.endEditing(true)
+    }
+    
 }
 
